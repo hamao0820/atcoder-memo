a97edfd9b66f71f1d8876bb58bc552511c685493
浜田　俊祐 <hamashun0820@icloud.com>
2023-11-09 12:30:46 +0900

    theme.config.tsxを更新

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Link from "next/link";
+import path from "path";
 
 const config: DocsThemeConfig = {
   logo: <span>hamaoの競プロメモ</span>,
-  project: {
-    link: "https://github.com/hamao0820/atcoder-memo",
-  },
   docsRepositoryBase: "https://github.com/hamao0820/atcoder-memo",
+  editLink: {
+    component: ({ children, className, filePath }) => {
+      const baseUrl = "https://github.com";
+      const url = new URL(
+        path.join("hamao0820", "atcoder-memo", "tree", "main", filePath),
+        baseUrl
+      );
+      return (
+        <Link href={url} className={className} target="_blank">
+          {children}
+        </Link>
+      );
+    },
+  },
   footer: {
     text: "MIT 2023 © hamao.",
   },
 };
 
-export default config
+export default config;
