65360f28631bc374c4d5ffe4a9d37c305d4f5015
浜田　俊祐 <hamashun0820@icloud.com>
2024-07-02 09:53:03 +0900

    タグ一覧ページを更新

--- tags/generateMd.ts
+++ tags/generateMd.ts
@@ -1,18 +1,28 @@
-import { getTags } from "./getTags";
+import { getTags, Tag } from "./getTags";
 
 let md = "# タグ一覧";
 const tags = getTags();
-tags.sort((a, b) => {
-  if (a.name < b.name) return -1;
-  if (a.name > b.name) return 1;
+tags
+  .sort((a, b) => {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
 
-  return 0;
-});
-tags.map((tag) => {
-  md += `\n\n## ${tag.name}\n`;
-  tag.pages.map((page) => {
-    md += `\n- [${page.title}](/${page.path})`;
+    return 0;
+  })
+  .map<Tag>((tag) => {
+    const pages = tag.pages.sort((a, b) => {
+      if (a.title < b.title) return -1;
+      if (a.title > b.title) return 1;
+
+      return 0;
+    });
+    return { ...tag, pages };
+  })
+  .forEach((tag) => {
+    md += `\n\n## ${tag.name}\n`;
+    tag.pages.forEach((page) => {
+      md += `\n- [${page.title}](/${page.path})`;
+    });
   });
-});
 
 console.log(md);
